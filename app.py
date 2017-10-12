2629fa6dafa37973c424f8df56e3b3124206d39b
Joyce Wu <jwu26@stuy.edu>
2017-10-11 22:02:24 -0400

    flashified and inheritance

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ HW08 - Do I Know You?
 """
 
 import os
-from flask import Flask, render_template, request, session, redirect, url_for, redirect
+from flask import Flask, render_template, request, session, redirect, url_for, redirect, flash
 
 app = Flask(__name__)
 #generates a random 32-bit string to act as the key
@@ -14,6 +14,18 @@ app.secret_key = os.urandom(32)
 #hardcoded username and password
 username = "dog"
 password = "cat"
+SUCCESS = 1
+BAD_PASS = -1
+BAD_USER = -2
+
+def auth(user, passw):
+    if user == username:
+        if passw == password:
+            return SUCCESS
+        else:
+            return BAD_PASS
+    else:
+        return BAD_USER
 
 @app.route("/", methods=["POST","GET"])
 def login():
@@ -25,6 +37,7 @@ def login():
 
 @app.route("/login", methods=["POST","GET"])
 def checklogin():
+    '''
     #wrong username
     if request.form["user"] != username:
         return render_template("login.html", loginfailed = "Wrong username!")
@@ -32,9 +45,19 @@ def checklogin():
     if request.form["pass"] != password:
         return render_template("login.html", loginfailed = "Wrong password!")
     #correct login, directs to welcome page
-    else:
+    '''
+
+    result = auth(request.form["user"], request.form["pass"])
+    flash("what?")
+    if result == SUCCESS:
         session["username"] = request.form["user"]
         return redirect(url_for("welcome_page"))
+    if result == BAD_PASS:
+        flash("password")
+        return render_template("login.html", loginfailed = "Wrong password!")
+    if result == BAD_USER:
+        flash("user")
+        return render_template("login.html", loginfailed = "Wrong password!")
 
 @app.route("/welcome", methods=["POST","GET"])
 def welcome_page():
@@ -42,6 +65,7 @@ def welcome_page():
     if "username" in session.keys():
         return render_template("welcome.html", user_name = session["username"])
     else:
+        flash("do they think they can get away without logging in?")
         return render_template("login.html", loginfailed= "Please login!")
 
 @app.route("/logout", methods=["POST","GET"])
